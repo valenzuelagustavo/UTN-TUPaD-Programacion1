2a7ef0cadd78157f3f3df35310c3db61275ef0cc
Gustavo Valenzuela <gustavo221b@hotmail.com>
2026-08-16 22:29:49 -0300

    Agregado el ejercicio 3 de la Unidad 5

---
+++ Unidad_5_Listas/practico_5_listas_3.py
@@ -0,0 +1,22 @@
+### EJERCIO 3 ###
+
+#Importo random para el manejo de los números aleatorios
+import random as rm
+#Listas
+num_pares = []
+num_impares = []
+lista_numeros = []
+#Añado los números aleatorios a la lista
+for numeros in range(15):
+    lista_numeros.append(rm.randint(1, 100))
+#Muestro la lista, no lo pide la consigna pero para controlar el resultado
+print(f"La lista de números aleatorios es: {lista_numeros}")
+#Recorro la lista y añado a las listad de impares o pares según corresponda
+for i in range(len(lista_numeros)):
+    if lista_numeros[i] % 2 == 0:
+        num_pares.append(i)
+    elif lista_numeros[i] % 2 != 0:
+        num_impares.append(i)
+#Informo la cantidad de pares e impares
+print(f"La lista de números impares tiene {len(num_impares)} números. La lista de números pares tiene {len(num_pares)} números.")
+
